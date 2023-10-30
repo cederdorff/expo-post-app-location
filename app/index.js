89367637d2f776ee7a02db33ab5d5992b945a0bc
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-10-30 15:28:57 +0100

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,19 +1,21 @@
 import { useRouter } from "expo-router";
-import "../firebase-config";
-import { onAuthStateChanged, getAuth } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { auth } from "../firebase-config";
 
 export default function AppRoot() {
     const router = useRouter();
-    const auth = getAuth();
 
-    onAuthStateChanged(auth, user => {
-        console.log(user);
-        if (user) {
-            // User is signed in
-            router.replace("/posts");
-        } else {
-            // User is signed out
-            router.replace("/sign-in");
-        }
-    });
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            console.log(user);
+            if (user) {
+                // User is signed in
+                router.replace("/posts");
+            } else {
+                // User is signed out
+                router.replace("/sign-in");
+            }
+        });
+    }, []);
 }
