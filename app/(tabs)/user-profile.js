220ce9793c452ad6828e7dd99a19828c80aa5a22
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-10-30 15:27:34 +0100

    How to read FB Auth object customised

--- app/(tabs)/user-profile.js
+++ app/(tabs)/user-profile.js
@@ -1,20 +1,34 @@
-import { Stack, useRouter } from "expo-router";
-import { getAuth, signOut } from "firebase/auth";
-import { useEffect, useState } from "react";
-import { Button, Platform, StyleSheet, Text, TextInput, View, Image, TouchableOpacity, ScrollView } from "react-native";
 import * as ImagePicker from "expo-image-picker";
+import { Stack, useRouter } from "expo-router";
+import { signOut } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { Button, Image, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { auth } from "../../firebase-config";
 
 export default function UserProfile() {
-    const auth = getAuth();
     const [name, setName] = useState("");
     const [title, setTitle] = useState("");
     const [mail, setMail] = useState("");
     const [image, setImage] = useState("");
     const router = useRouter();
+    const url = `https://expo-post-app-default-rtdb.firebaseio.com/users/${auth.currentUser?.uid}.json`;
 
     useEffect(() => {
-        console.log(auth.currentUser.email);
         setMail(auth.currentUser.email);
+
+        async function getUser() {
+            const response = await fetch(url);
+            const userData = await response.json();
+            console.log(userData);
+
+            if (userData) {
+                // if userData exists set states with values from userData (data from firestore)
+                setName(userData.name);
+                setTitle(userData.title || "");
+                setImage(userData.image || imgPlaceholder);
+            }
+        }
+        getUser();
     }, []);
 
     async function handleSignOut() {
@@ -33,6 +47,22 @@ export default function UserProfile() {
         if (!result.canceled) {
             const base64 = "data:image/jpeg;base64," + result.assets[0].base64;
             setImage(base64);
+        }
+    }
+
+    async function handleSaveUser() {
+        const userToUpdate = { name: name, mail: mail, title, image }; // create an object to hold the user to update properties
+        console.log(userToUpdate);
+
+        const response = await fetch(url, {
+            method: "PUT",
+            body: JSON.stringify(userToUpdate)
+        });
+        if (response.ok) {
+            const data = await response.json();
+            console.log("User updated: ", data);
+        } else {
+            console.log("Sorry, something went wrong");
         }
     }
 
@@ -68,6 +98,15 @@ export default function UserProfile() {
                     placeholder="Type your mail"
                     autoCapitalize="none"
                 />
+
+                <Text style={styles.label}>Title</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={setTitle}
+                    value={title}
+                    placeholder="Type your mail"
+                    autoCapitalize="none"
+                />
                 <Text style={styles.label}>Mail</Text>
                 <TextInput
                     style={styles.input}
@@ -77,16 +116,9 @@ export default function UserProfile() {
                     autoCapitalize="none"
                     editable={false}
                 />
-                <Text style={styles.label}>Title</Text>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={setTitle}
-                    value={title}
-                    placeholder="Type your mail"
-                    autoCapitalize="none"
-                />
-
-                <Button title="Save" color="#264c59" />
+                <View style={styles.buttonContainer}>
+                    <Button title="Save" color="#264c59" onPress={handleSaveUser} />
+                </View>
             </View>
         </ScrollView>
     );
@@ -109,9 +141,12 @@ const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: "#ffffff",
         borderRadius: 20,
-        marginVertical: 20
+        marginVertical: 10
     },
     image: {
         aspectRatio: 1
+    },
+    buttonContainer: {
+        marginBottom: 50
     }
 });
