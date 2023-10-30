eac8bb9d4eee3f5ce1361f2a28251d302e3ae740
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-10-30 07:25:56 +0100

    FB updated

--- app/index.js
+++ app/index.js
@@ -1,6 +1,18 @@
-import { Redirect } from "expo-router";
+import { useRouter } from "expo-router";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase-config";
 
 export default function AppRoot() {
-    return <Redirect href="/posts" />;
+    const router = useRouter();
+
+    onAuthStateChanged(auth, user => {
+        console.log(user);
+        if (user) {
+            // User is signed in
+            router.replace("/posts");
+        } else {
+            // User is signed out
+            router.replace("/sign-in");
+        }
+    });
 }
