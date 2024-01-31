4f9dd53d8497bb74386751b7fd825ccdbd7429ef
Rasmus Cederdorff <rasmus@cederdorff.com>
2024-01-31 18:35:17 +0100

    Update Post.js

--- app/components/Post.js
+++ app/components/Post.js
@@ -1,7 +1,14 @@
 import { useActionSheet } from "@expo/react-native-action-sheet";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
-import { Alert, Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+    Alert,
+    Image,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View
+} from "react-native";
 import Avatar from "./Avatar";
 
 export default function Post({ post, reload }) {
@@ -60,17 +67,23 @@ export default function Post({ post, reload }) {
     }
 
     function showDeleteDialog() {
-        Alert.alert("Delete Post", `Do you want to delete post '${post.caption}'?`, [
-            {
-                text: "No",
-                style: "destructive"
-            },
-            { text: "Yes", onPress: deletePost }
-        ]);
+        Alert.alert(
+            "Delete Post",
+            `Do you want to delete post '${post.caption}'?`,
+            [
+                {
+                    text: "No",
+                    style: "destructive"
+                },
+                { text: "Yes", onPress: deletePost }
+            ]
+        );
     }
 
     async function deletePost() {
-        const response = await fetch(`${API_URL}/posts/${post.id}.json`, { method: "DELETE" });
+        const response = await fetch(`${API_URL}/posts/${post.id}.json`, {
+            method: "DELETE"
+        });
         if (response.ok) {
             console.log("Post deleted!");
             reload();
@@ -82,14 +95,18 @@ export default function Post({ post, reload }) {
             <View style={styles.headerContainer}>
                 <Avatar userId={post.uid} />
                 <TouchableOpacity style={styles.dots} onPress={showEditMenu}>
-                    <Ionicons name="ellipsis-horizontal" size={28} color="#264c59" />
+                    <Ionicons
+                        name="ellipsis-horizontal"
+                        size={28}
+                        color="#264c59"
+                    />
                 </TouchableOpacity>
             </View>
             <Image style={styles.image} source={{ uri: post.image }} />
             <Text style={styles.caption}>{post.caption}</Text>
-            {post.location.city ? (
+            {post.location?.city ? (
                 <Text style={styles.city}>
-                    {post.location.city}, {post.location.country}
+                    {post.location?.city}, {post.location?.country}
                 </Text>
             ) : null}
             <Text style={styles.date}>{formatDate(post.createdAt)}</Text>
